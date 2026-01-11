75be2642b84c925ecd3d78c4b54f37a38ad3b0e8
beriman <beriman.juliano@gmail.com>
2026-01-11 09:52:34 +0700

    login

---
+++ src/app/(public)/login/layout.tsx
@@ -0,0 +1,8 @@
+// Layout khusus untuk halaman login - tanpa header/footer public
+export default function LoginLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <>{children}</>;
+}
